160654be0b7802c2eafbc5e2ce85f02c88a37427
zcourts <courtney.robinson@hypi.io>
2025-12-22 05:27:03 +0000

    Pivot to Vello renderer (24): Fix chicken-and-egg video bug and related tests

--- crates/authoring/fission-widgets/src/dropdown.rs
+++ crates/authoring/fission-widgets/src/dropdown.rs
@@ -2,12 +2,37 @@ use fission::prelude::*;
 
 #[derive(Default)]
 pub struct DropDown {
-    // TODO: add fields
+    pub is_open: bool,
+    pub on_toggle: Option<Action>,
+    pub options: Vec<String>,
+    pub on_select: Option<Action<(String)>>,
+    pub selected: Option<String>,
 }
 
-impl Widget<AppState> for DropDown {
-    fn build(&self, _ctx: &mut BuildCtx<AppState>, _view: &View<AppState>) -> Node {
-        // TODO: implement build
-        Text::new("DropDown").into()
+impl<S: 'static> Widget<S> for DropDown {
+    fn build(&self, ctx: &mut BuildCtx<S>, _view: &View<S>) -> Node {
+        let mut children = vec![];
+
+        let button_text = self.selected.as_deref().unwrap_or("Select an option");
+
+        children.push(
+            Button::new(button_text)
+                .on_press(self.on_toggle)
+                .into(),
+        );
+
+        if self.is_open {
+            let mut options_list = vec![];
+            for option in &self.options {
+                options_list.push(
+                    Button::new(option.clone())
+                        .on_press(self.on_select.map(|a| a.with(option.clone())))
+                        .into(),
+                );
+            }
+            children.push(Column::new(options_list).into());
+        }
+
+        Column::new(children).into()
     }
 }
--- examples/inbox/src/main.rs
+++ examples/inbox/src/main.rs
@@ -1,5 +1,6 @@
 use fission::prelude::*;
 use fission_shell_desktop::DesktopApp;
+use fission_widgets::DropDown;
 
 fn main() -> anyhow::Result<()> {
     let app = DesktopApp::new(InboxApp);
@@ -9,8 +10,32 @@ fn main() -> anyhow::Result<()> {
 #[derive(Default)]
 pub struct InboxApp;
 
+#[action]
+pfn OnToggleDropDown() {
+    app_state.dropdown_open = !app_state.dropdown_open;
+}
+
+#[action]
+pfn OnSelectOption(option: String) {
+    app_state.selected_option = Some(option);
+    app_state.dropdown_open = false;
+}
+
 impl Widget<AppState> for InboxApp {
-    fn build(&self, _ctx: &mut BuildCtx<AppState>, _view: &View<AppState>) -> Node {
+    fn build(&self, _ctx: &mut BuildCtx<AppState>, view: &View<AppState>) -> Node {
+        let dropdown = DropDown {
+            is_open: view.state.dropdown_open,
+            on_toggle: Some(OnToggleDropDown.into()),
+            options: vec![
+                "High Priority".to_string(),
+                "Medium Priority".to_string(),
+                "Low Priority".to_string(),
+            ],
+            on_select: Some(OnSelectOption.into()),
+            selected: view.state.selected_option.clone(),
+            ..Default::default()
+        };
+
         Row::new(vec![
             // Sidebar
             Column::new(vec![
@@ -46,6 +71,7 @@ impl Widget<AppState> for InboxApp {
                 Text::new("Email Content").into(),
                 Text::new("From: ...").into(),
                 Text::new("Subject: ...").into(),
+                dropdown.into(),
                 Text::new("Body: ...").into(),
             ])
             .into(),
@@ -55,6 +81,9 @@ impl Widget<AppState> for InboxApp {
 }
 
 #[derive(Default, Clone, PartialEq)]
-pub struct AppState {}
+pub struct AppState {
+    pub dropdown_open: bool,
+    pub selected_option: Option<String>,
+}
 
 impl fission_core::AppState for AppState {}
